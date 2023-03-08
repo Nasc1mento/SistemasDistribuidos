d3bfd1422863881914f1b5e6925b0b7ea31ab248
Adryan N. Reis <88512599+Nasc1mento@users.noreply.github.com>
2023-03-07 21:02:14 -0300

    Update client.py

--- aula-07-03-2023/client.py
+++ aula-07-03-2023/client.py
@@ -3,18 +3,16 @@ import socket
 client = socket.socket()
 client.connect(('0.0.0.0',7550)) # colocando o ip do servidor e a porta que abri lá
 
-
-while True:
     
-    numeros = input('') # abrindo um input para envio dos numeros, separe por virgula para funcionar
+numeros = input('') # abrindo um input para envio dos numeros, separe por virgula para funcionar
 
-    client.send(bytes(numeros, 'ascii') ) # trasformado a string ( o input ) para byte e enviando
+client.send(bytes(numeros, 'ascii') ) # trasformado a string ( o input ) para byte e enviando
 
-    result = str(client.recv(1024),'ascii') # recebendo a resposta que o cliente enviou, transformando de byte para string
+result = str(client.recv(1024),'ascii') # recebendo a resposta que o cliente enviou, transformando de byte para string
 
-    print('A soma é:',result) # printando o resultado
+print('A soma é:',result) # printando o resultado
       
-    client.close() # fechando a conexão
+client.close() # fechando a conexão
 
 
 
